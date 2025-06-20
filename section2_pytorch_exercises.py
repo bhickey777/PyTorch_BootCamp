398c091783da8bc830df5a15961b35208dcb88fa
Brian Hickey <90266950+bhickey777@users.noreply.github.com>
2025-06-20 18:31:30 -0500

    Added additianal excercises from section 2

--- section2_pytorch_exercises.py
+++ section2_pytorch_exercises.py
@@ -1,8 +1,28 @@
 import torch
-import time
+
 
 # Can also import the common abbreviation "nn" for "Neural Networks"
 from torch import nn
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
+RANDOM_SEED = 64
+torch.manual_seed = RANDOM_SEED
+
+a = torch.rand(1, 3, 5)
+
+torch.manual_seed = RANDOM_SEED
+b = torch.rand(1, 3, 5)
+
+c = a * b
+
+torch.manual_seed = RANDOM_SEED
+d = torch.rand(1, 5, 5)
+
+torch.manual_seed = RANDOM_SEED
+e = torch.matmul(a, d)
+
+print(c == e)
+
+print(torch.min(e))
+print(torch.max(e))
