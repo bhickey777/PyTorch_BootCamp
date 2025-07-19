Fixed model loop and added softmax activation function

03_multiclass_classification.py
@@ -12,7 +12,7 @@ from helper_function import plot_predictions, plot_decision_boundary
 
 RANDOM_SEED = 42
 NUM_CLASSES = 4
-NUM_FEATURES = 3
+NUM_FEATURES = 2
 torch.manual_seed(RANDOM_SEED)
 
 # Can use this when using CUDA
@@ -31,8 +31,9 @@ X, y = make_blobs(n_samples,
                 cluster_std=1.5,
                 random_state=RANDOM_SEED)
 
+#convert numpy arrays to float
 X = torch.from_numpy(X).type(torch.float)
-y = torch.from_numpy(y).type(torch.float)
+y = torch.from_numpy(y).type(torch.LongTensor)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -46,6 +47,12 @@ print(y_train[:10])
 plt.figure(figsize=(10, 7))
 plt.scatter(X[:, 0], X[:, 1], c=y,cmap=plt.cm.RdYlBu)
 # plt.show()
+
+#Uses activation function torch.softmax
+def convertToLabels(logits, dim=1):
+    pred_probs = torch.softmax(logits, dim) #prediction probabilities
+    preds = torch.argmax(pred_probs, dim) #prediction labels
+    return preds
 
 class BlobModel(nn.Module):
     def __init__(self, input_size, output_size=4, hidden_size=8):
@@ -63,27 +70,50 @@ class BlobModel(nn.Module):
 
 model0 = BlobModel(input_size=NUM_FEATURES).to(device)
 
-loss = nn.CrossEntropyLoss()
+loss_fn = nn.CrossEntropyLoss()
 
 #Setup an optimizer (ways to adjust the parameters)
-optimizer = torch.optim.Adam(
+optimizer = torch.optim.SGD(
         params = model0.parameters(), 
-        lr=0.001)
+        lr=0.1)
+
+#Get some prediction probabilites and see what the output looks like (logits)
+#Need to convert preds/logits to prediction probabilities and then to prediction labels
+model0.eval()
+with torch.inference_mode():
+    y_logits = model0(X_test.to(device))
+    #convert to labels ala activation function
+    y_preds = convertToLabels(y_logits)
+    print(y_preds[:10])
 
 epochs = 1000
 for epoch in range(epochs):
     model0.train()
-    y_pred = model0(X_train)
-    optimizer.zero_grad()                 #3
-    loss.backward()                       #4
-    optimizer.step()                      #5
+    y_logits = model0(X_train)
+    y_pred = convertToLabels(y_logits)
+    loss = loss_fn(y_logits, y_train) #pass in logits to get loss not the preds
+    acc = accuracy_fn(y_true=y_train, y_pred=y_pred)
+
+    optimizer.zero_grad()                 
+    loss.backward()                      
+    optimizer.step()                      
 
     model0.eval()
     with torch.inference_mode():
-        test_logits = model0(X_test).squeeze()
-        test_pred = torch.round(torch.sigmoid(test_logits))
+        test_logits = model0(X_test)
+        test_pred = convertToLabels(test_logits)
         test_loss = loss_fn(test_logits, y_test)
         test_acc = accuracy_fn(y_true=y_test, y_pred=test_pred)
         # print out results
         if epoch % 10 == 0:
-            print(f"Epoch: {epoch} | Loss: {loss:.5f} | Acc: {acc:.2f}% | Test loss: {test_loss:.5f} | Test acc: {test_acc: .2f}%")
+            print(f"Epoch: {epoch} | Loss: {loss:.4f} | Acc: {acc:.2f}% | Test loss: {test_loss:.4f} | Test acc: {test_acc: .2f}%")
+
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.title("Train")
+plot_decision_boundary(model0, X_train, y_train)
+plt.subplot(1, 2, 2)
+plt.title("Test")
+plot_decision_boundary(model0, X_test, y_test)
+
+plt.show()